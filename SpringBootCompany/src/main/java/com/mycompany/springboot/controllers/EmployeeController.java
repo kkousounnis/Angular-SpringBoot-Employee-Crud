30c3480f58d1ee7235b6dd2444a29a359c2035f7
Konstantinos <kwstas654321@gmail.com>
2021-02-09 21:49:13 +0200

    All employees print to front end

--- SpringBootCompany/src/main/java/com/mycompany/springboot/controllers/EmployeeController.java
+++ SpringBootCompany/src/main/java/com/mycompany/springboot/controllers/EmployeeController.java
@@ -4,6 +4,7 @@ import com.mycompany.springboot.entities.Employee;
 import com.mycompany.springboot.services.EmployeeServiceImpl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,8 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeServiceImpl employeeService;
-
+    
+    @CrossOrigin(origins = "http://localhost:4200")    
     @GetMapping("/employee")
     public List<Employee> get() {
         return employeeService.get();
