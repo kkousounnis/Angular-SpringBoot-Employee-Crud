06b535ab1a382a614882890f2250d55d41038fc3
Konstantinos <kwstas654321@gmail.com>
2021-02-11 12:29:29 +0200

    Propoer nav bar

--- FrontEndCompany/src/app/app-routing.module.ts
+++ FrontEndCompany/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { CreateEmployeeComponent} from './create-employee/create-employee.component';
+
 
 const routes: Routes = [
   {path: 'employees', component: EmployeeListComponent},
+  {path: 'create-employee', component: CreateEmployeeComponent},
   {path: '', redirectTo: 'employees', pathMatch: 'full'}
 ];
 
